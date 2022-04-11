a52e790c1667e9ea1b77fe1cbc4d1a16e9ee5c66
Mattia Fasoli <fasoxy@gmail.com>
2022-04-11 13:49:26 +0200

    New Commit

--- Exercise01.playground/Contents.swift
+++ Exercise01.playground/Contents.swift
@@ -1,3 +1,7 @@
+/*
+ 
+// OPTIONAL BINDING
+
 func Pippo(string: String?) -> (lengthString: Int, uppercaseString: String) {
     
     if let stringNotNull = string {
@@ -13,3 +17,27 @@ print(Pippo(string: "hello world").uppercaseString)
 
 print(Pippo(string: nil).lengthString)
 print(Pippo(string: nil).uppercaseString)
+
+*/
+
+/*
+ 
+// IMPLICITLY UNWRAPPED OPTIONALS
+
+func Pippo(string: String!) -> (lengthString: Int, uppercaseString: String) {
+    
+    if string != nil {
+        return (string.count, string.uppercased())
+    } else {
+        return (-1, "Non so")
+    }
+    
+}
+
+print(Pippo(string: "hello world").lengthString)
+print(Pippo(string: "hello world").uppercaseString)
+
+print(Pippo(string: nil).lengthString)
+print(Pippo(string: nil).uppercaseString)
+
+*/

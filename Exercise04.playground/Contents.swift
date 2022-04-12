3fb2ff710d3b218448360caf115f5dd77a0d8285
Mattia Fasoli <fasoxy@gmail.com>
2022-04-12 16:59:55 +0200

    New Commit

--- Exercise04.playground/Contents.swift
+++ Exercise04.playground/Contents.swift
@@ -1,3 +1,33 @@
-func functionOne(stringOne: String, stringTwo: String) -> Bool {
+func equal(stringOne: String, stringTwo: String) -> Bool {
+    
+    if stringOne == stringTwo {
+        return true
+    } else {
+        return false
+    }
     
 }
+ 
+func equalIgnoreCase(stringOne: String, stringTwo: String) -> Bool {
+    
+    if stringOne.lowercased() == stringTwo.lowercased() {
+        return true
+    } else {
+        return false
+    }
+    
+}
+
+func similar2(stringOne: String, stringTwo: String) -> Bool {
+    
+    if stringOne.prefix(2) == stringTwo.prefix(2) {
+        return true
+    } else {
+        return false
+    }
+    
+}
+
+print(equal(stringOne: "puzzeTta", stringTwo: "puzzetta"))
+print(equalIgnoreCase(stringOne: "puzzeTta", stringTwo: "puzzetta"))
+print(similar2(stringOne: "puzzetta", stringTwo: "puzzetta"))

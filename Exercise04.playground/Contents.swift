0aaa44e6f41011dc4ef78a2b9d29598d2d883c2b
Mattia Fasoli <fasoxy@gmail.com>
2022-04-12 17:17:21 +0200

    New Commit

--- Exercise04.playground/Contents.swift
+++ Exercise04.playground/Contents.swift
@@ -31,3 +31,23 @@ func similar2(stringOne: String, stringTwo: String) -> Bool {
 print(equal(stringOne: "puzzeTta", stringTwo: "puzzetta"))
 print(equalIgnoreCase(stringOne: "puzzeTta", stringTwo: "puzzetta"))
 print(similar2(stringOne: "puzzeTta", stringTwo: "puzzetta"))
+
+func countSimilar(string: String, array: [String], function: (String, String) -> Bool) -> Int {
+    
+    var count: Int = 0
+    
+    for a in array {
+        
+        if function(string, a) {
+            count = count + 1
+        }
+        
+    }
+    
+    return count
+    
+}
+
+print(countSimilar(string: "aaa", array: ["aaa", "Aaa", "aaA"], function: equal))
+print(countSimilar(string: "aaa", array: ["aaa", "Aaa", "aaA"], function: equalIgnoreCase))
+print(countSimilar(string: "aaa", array: ["aaa", "Aaa", "aaA"], function: similar2))

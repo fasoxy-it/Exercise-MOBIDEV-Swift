83ac03ee1288b57eb4f852f002c979657132ac74
Mattia Fasoli <fasoxy@gmail.com>
2022-04-13 18:28:41 +0200

    New Commit

--- Exercise06.playground/Contents.swift
+++ Exercise06.playground/Contents.swift
@@ -2,22 +2,13 @@ class Pearson {
     var name: String = "Mattia"
     var surname: String = "Fasoli"
     
-    var fullName: (String, String) {
+    var fullName: (String) {
         get {
-            return (name, surname)
-        }
-        set {
-            name = newValue.0
-            surname = newValue.1
+            return "\(name) \(surname)"
         }
     }
 }
 
 var pearson = Pearson()
-
-print("\(pearson.fullName.0) \(pearson.fullName.1)")
-
-pearson.fullName = ("Rebecca", "Paiardi")
-
-print("\(pearson.fullName.0) \(pearson.fullName.1)")
+print("\(pearson.fullName)")
 

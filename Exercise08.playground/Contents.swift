f363b2da394f1cd57a8fa57852a684b7e837f54a
Mattia Fasoli <fasoxy@gmail.com>
2022-04-22 17:16:49 +0200

    New  Commit

---
+++ Exercise08.playground/Contents.swift
@@ -0,0 +1,44 @@
+class Pearson {
+    var name: String = "Mattia"
+    var surname: String = "Fasoli"
+    
+    var fullName: (String) {
+        get {
+            return "\(name) \(surname)"
+        }
+    }
+}
+
+class SuperHero: Pearson {
+    var power: String = "fa puzzette" {
+        willSet {
+            print("Sto per cambiare il valore da \(power) a \(newValue)")
+        }
+    }
+    
+    var superPower: (String) {
+        get {
+            return "\(name) \(surname) \(power)"
+        }
+    }
+}
+
+var pearson1 = Pearson()
+pearson1.name = "Mattia"
+pearson1.surname = "Fasoli"
+
+var pearson2 = Pearson()
+pearson2.name = "Rebecca"
+pearson2.surname = "Paiardi"
+
+var superhero = SuperHero()
+superhero.name = "Stefano"
+superhero.surname = "Paiardi"
+superhero.power = "suona"
+
+var world = [pearson1, pearson2, superhero]
+
+for index in world {
+    print(index)
+}
+

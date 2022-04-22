ba4d5f27f3f08b09a12bc3102864784dbb9ab4c3
Mattia Fasoli <fasoxy@gmail.com>
2022-04-22 18:56:24 +0200

    New Commit

--- Exercise09.playground/Contents.swift
+++ Exercise09.playground/Contents.swift
@@ -1,3 +1,39 @@
-import UIKit
+class Person {
+    var name: String
+    var surname: String
+    
+    init(name: String, surname: String) {
+        self.name = name
+        self.surname = surname
+    }
+    
+    var fullName: (String) {
+        get {
+            return "\(name) \(surname)"
+        }
+    }
+}
 
-var greeting = "Hello, playground"
+protocol Worker {
+    func doWork()
+}
+
+class Employee: Person, Worker {
+    var salary: Int
+    
+    func doWork() {
+        print("Ok, basta che mi paghi \(salary)")
+    }
+    
+    init(name: String, surname: String, salary: Int) {
+        self.salary = salary
+        super.init(name: name, surname: surname)
+    }
+}
+
+var employee = Employee(name: "Mattia", surname: "Fasoli", salary: 2500)
+
+employee.doWork()
+
+
+

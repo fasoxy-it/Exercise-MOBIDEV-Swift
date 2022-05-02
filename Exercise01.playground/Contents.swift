64be5f46b67a40c3fb71bd3c6dbd26ff0c51aee9
Mattia Fasoli <fasoxy@gmail.com>
2022-05-02 14:54:08 +0200

    New Commit

--- Exercise01.playground/Contents.swift
+++ Exercise01.playground/Contents.swift
@@ -1,43 +1,79 @@
 /*
- 
-// OPTIONAL BINDING
 
-func Pippo(string: String?) -> (lengthString: Int, uppercaseString: String) {
-    
-    if let stringNotNull = string {
-        return (stringNotNull.count, stringNotNull.uppercased())
+OPTIONAL BINDING
+ 
+ func Pippo(string: String?) -> (lengthString: Int, upppercaseString: String) {
+     if let stringNotNil = string {
+         return (stringNotNil.count, stringNotNil.uppercased())
+     } else {
+         return (-1, "Non so")
+     }
+ }
+
+ print(Pippo(string: "hello world").lengthString)
+ print(Pippo(string: "hello world").upppercaseString)
+
+ print(Pippo(string: nil).lengthString)
+ print(Pippo(string: nil).upppercaseString)
+ 
+*/
+
+/*
+
+FORCED UNWRAPPING
+
+func Pippo(string: String?) -> (lengthString: Int, upppercaseString: String) {
+    if string != nil {
+        return (string!.count, string!.uppercased())
     } else {
         return (-1, "Non so")
     }
-    
 }
 
 print(Pippo(string: "hello world").lengthString)
-print(Pippo(string: "hello world").uppercaseString)
+print(Pippo(string: "hello world").upppercaseString)
 
 print(Pippo(string: nil).lengthString)
-print(Pippo(string: nil).uppercaseString)
+print(Pippo(string: nil).upppercaseString)
 
 */
 
 /*
  
-// IMPLICITLY UNWRAPPED OPTIONALS
-
-func Pippo(string: String!) -> (lengthString: Int, uppercaseString: String) {
-    
+//IMPLICITLY UNWRAPPED OPTIONALS
+ 
+func Pippo(string: String!) -> (lengthString: Int, upppercaseString: String) {
     if string != nil {
         return (string.count, string.uppercased())
     } else {
         return (-1, "Non so")
     }
-    
 }
 
 print(Pippo(string: "hello world").lengthString)
-print(Pippo(string: "hello world").uppercaseString)
+print(Pippo(string: "hello world").upppercaseString)
 
 print(Pippo(string: nil).lengthString)
-print(Pippo(string: nil).uppercaseString)
+print(Pippo(string: nil).upppercaseString)
 
 */
+
+/*
+
+// Nil-Coalescing Operator
+ 
+let defaultString = (-1, "Non so")
+
+func Pippo(string: String?) -> (Int, String) {
+    return (string.count, string.uppercased()) ?? defaultString
+}
+
+print(Pippo(string: "hello world").lengthString)
+print(Pippo(string: "hello world").upppercaseString)
+
+print(Pippo(string: nil).lengthString)
+print(Pippo(string: nil).upppercaseString)
+
+*/
+
+
